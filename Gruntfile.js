36895912aca1135f160098be7abc19d703d14bfd
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-10-11 15:26:39 -0300

    added Gruntfile and gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+    var path = require('path');
+
+    require('load-grunt-config')(grunt, {
+        init: true,
+        configPath: path.join(process.cwd(), 'tasks/gruntjs'),
+        data: {
+        	projectDev : './',
+        	projectDir : 'dist/',
+            pkg: grunt.file.readJSON('package.json')
+        }
+    });
+
+};
